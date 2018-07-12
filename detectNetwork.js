eb265a6de7b5769953db7775316e2ae3f0661c97
Christopher Tung <cstung21@gmail.com>
2018-07-11 19:14:53 -0700

    Add China UnionPay & Switch to detectNetwork.js

--- detectNetwork.js
+++ detectNetwork.js
@@ -19,14 +19,18 @@ var detectNetwork = function(cardNumber) {
   	network = 'Diner\'s Club';
   } else if ((cardNumber.slice(0,2) === '34' || cardNumber.slice(0,2) === '37') && cardNumber.length === 15) {
   	network = 'American Express';
+  } else if ((cardNumber.slice(0,4) === '4903' || cardNumber.slice(0,4) === '4905' || cardNumber.slice(0,4) === '4911' || cardNumber.slice(0,4) === '4936' || cardNumber.slice(0,6) === '564182' || cardNumber.slice(0,6) === '633110' || cardNumber.slice(0,4) === '6333' || cardNumber.slice(0,4) === '6759') && (cardNumber.length === 16 || cardNumber.length === 18 || cardNumber.length === 19)) {
+    network = 'Switch';
   } else if (cardNumber.slice(0,1) === '4' && (cardNumber.length === 13 || cardNumber.length === 16 || cardNumber.length === 19)) {
   	network = 'Visa';
-  } else if ((cardNumber.slice(0,2) === '51' || cardNumber.slice(0,2) === '52' || cardNumber.slice(0,2) === '53' || cardNumber.slice(0,2) === '54' || cardNumber.slice(0,2) === '55') && cardNumber.length === 16) {
+  } else if ((parseInt(cardNumber.slice(0,2)) >= 51 && parseInt(cardNumber.slice(0,2)) <= 55) && cardNumber.length === 16) {
   	network = 'MasterCard';
-  } else if ((cardNumber.slice(0,4) === '6011' || cardNumber.slice(0,3) === '644' || cardNumber.slice(0,3) === '645' || cardNumber.slice(0,3) === '646' || cardNumber.slice(0,3) === '647' || cardNumber.slice(0,3) === '648' || cardNumber.slice(0,3) === '649' || cardNumber.slice(0,2) === '65') && (cardNumber.length === 16 || cardNumber.length === 19)) {
+  } else if ((cardNumber.slice(0,4) === '6011' || (parseInt(cardNumber.slice(0,3)) >= 644 && parseInt(cardNumber.slice(0,3)) <= 649) || cardNumber.slice(0,2) === '65') && (cardNumber.length === 16 || cardNumber.length === 19)) {
   	network = 'Discover';
   } else if ((cardNumber.slice(0,4) === '5018' || cardNumber.slice(0,4) === '5020' || cardNumber.slice(0,4) === '5038' || cardNumber.slice(0,4) === '6304') && (cardNumber.length >= 12 && cardNumber.length <= 19)) {
   	network = 'Maestro';
+  } else if (((parseInt(cardNumber.slice(0,6)) >= 622126 && parseInt(cardNumber.slice(0,6)) <= 622925) || (parseInt(cardNumber.slice(0,3)) >= 624 && parseInt(cardNumber.slice(0,3)) <= 626) || (parseInt(cardNumber.slice(0,4)) >= 6282 && parseInt(cardNumber.slice(0,4)) <= 6288)) && (cardNumber.length >= 16 && cardNumber.length <= 19)) {
+    network = 'China UnionPay';
   }
 
   return network;
